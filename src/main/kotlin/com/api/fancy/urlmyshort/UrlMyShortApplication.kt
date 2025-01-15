fcf03d22bd86c8839fea937eb24e4f596fef45d0
de-meyer <dmeyer.private@gmail.com>
2025-01-15 21:29:48 +0100

    initial commit

---
+++ src/main/kotlin/com/api/fancy/urlmyshort/UrlMyShortApplication.kt
@@ -0,0 +1,11 @@
+package com.api.fancy.urlmyshort
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UrlMyShortApplication
+
+fun main(args: Array<String>) {
+    runApplication<UrlMyShortApplication>(*args)
+}
---
+++ src/test/kotlin/com/api/fancy/urlmyshort/UrlMyShortApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.api.fancy.urlmyshort
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UrlMyShortApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
